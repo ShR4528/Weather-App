d2ca20f9781031df752339a841c6e94ed3b66bb0
ShR4528 <107538948+ShR4528@users.noreply.github.com>
2022-09-30 10:21:19 +0300

    first

---
+++ src/Components/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => (
+    <div>
+      <h1>Weather Finder</h1>
+       <p>Find out Temperature, conditions and more...</p>
+    </div>
+
+)
+
+export default Title;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import "./App.css";
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
